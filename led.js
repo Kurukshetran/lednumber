8fe91eabf56a1e6685eb04266c1d20990a2309ad
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2013-02-12 22:55:22 +0900

    角が丸くなった

--- led.js
+++ led.js
@@ -5,7 +5,7 @@ jQuery.event.add(window, "load", function(){
     var canvas = target;
     var context = canvas.getContext('2d');
     var color = [255,0,0];
-    var canvasSize = $('body').width() * 0.8;
+    var canvasSize = $('body').width() * 0.9;
 
     canvas.width = canvasSize;
     canvas.height = canvasSize / 3;
@@ -15,33 +15,31 @@ jQuery.event.add(window, "load", function(){
 
     for(var i=0; i<10; i++){
         led[i.toString()] = new SevenSegment(target, 20 + (size * 1.2)*i, 30, size);
-        led[i.toString()].setColor(255,24*i,24*i);
+        led[i.toString()].setColor(255,2,2);
         led[i.toString()].draw(led[i.toString()].mapping(i));
     }
 
-    new SeparatorSegment(target, 20, 200,size/3,size).draw(':');
-    new SeparatorSegment(target, 20 + 10 + size, 200,size/3,size).draw('.');
+    new SeparatorSegment(target, 20, 200, size).draw(':');
+    new SeparatorSegment(target, 20 + 10 + size, 200,size).draw('.');
 
 });
 
-//{{{
+//{{{ function SeparatorSegment(canvas, x,y,size){
 // period
-// comma
 // colon
-// semicolon
-function SeparatorSegment(canvas, x,y,width,height){
+function SeparatorSegment(canvas, x,y,size){
     this.canvas = canvas;
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
+    this.width = size*0.3;
+    this.height = size;
     this.color = [255,50,50];
     var context = canvas.getContext('2d');
-    context.rect(x, y, width, height);
+    context.rect(x, y, this.width, this.height);
     context.stroke();
 }
 //}}}
-//{{{
+//{{{ SeparatorSegment.prototype.draw = function(type, flg){
 SeparatorSegment.prototype.draw = function(type, flg){
     var context = this.canvas.getContext('2d');
     var rgb = this.color[0] + ',' + this.color[1] + ',' + this.color[2];
@@ -84,6 +82,7 @@ SeparatorSegment.prototype.draw = function(type, flg){
 }
 //}}}
 
+//{{{ function SevenSegment(canvas, x, y, size){
 //   a
 // f   b
 //   g
@@ -91,33 +90,47 @@ SeparatorSegment.prototype.draw = function(type, flg){
 //   d
 function SevenSegment(canvas, x, y, size){
     this.canvas = canvas;
-    this.width = size;
-    this.height = size * 1.8;
+    this.width = size*0.6;
+    this.height = size;
     this.x = x;
     this.y = y;
+
     var lx = x;//上
     var ty = y;//左
     var rx = x + this.width;//右
     var by = y + this.height;//下
+    var context = canvas.getContext('2d');
+
     this.color = [255,50,50];
-    context = canvas.getContext('2d');
     //context.rect(x, y, this.width, this.height);
     //context.stroke();
 
-    var bd = size * 0.15;
+    var bd = size * 0.10;
     var cy = ty + this.height / 2;
 
     this.point = {
-        a:[ [lx,ty], [rx,ty],[rx-bd,ty+bd],[lx+bd,ty+bd] ],
-        b:[ [rx,ty], [rx,cy-bd],[rx-bd/2,cy],[rx-bd,cy-bd],[rx-bd,ty+bd]],
-        c:[ [rx,cy+bd],[rx,by],[rx-bd,by-bd],[rx-bd,cy+bd],[rx-bd/2,cy]],
-        d:[ [lx,by], [rx,by],[rx-bd,by-bd],[lx+bd,by-bd]],
-        e:[ [lx+bd/2,cy], [lx+bd,cy+bd],[lx+bd,by-bd],[lx,by],[lx,cy+bd]],
-        f:[ [lx,ty],[lx+bd,ty+bd],[lx+bd,cy-bd],[lx+bd/2,cy],[lx,cy-bd]],
-        g:[ [lx+bd/2,cy],[lx+bd,cy-bd],[rx-bd,cy-bd],[rx-bd/2,cy],[rx-bd,cy+bd],[lx+bd,cy+bd]],
+        a:[ [lx+bd/2,ty+bd/2], [lx+bd,ty], [rx-bd,ty],    [rx-bd/2,ty+bd/2], [rx-bd,ty+bd],   [lx+bd,ty+bd] ],
+        b:[ [rx-bd/2,ty+bd/2], [rx,ty+bd], [rx,cy-bd/2],       [rx-bd/2,cy], [rx-bd,cy-bd/2], [rx-bd,ty+bd] ],
+        c:[ [rx,cy+bd/2],      [rx,by-bd], [rx-bd/2,by-bd/2], [rx-bd,by-bd], [rx-bd,cy+bd/2], [rx-bd/2,cy]  ],
+        d:[ [lx+bd/2,by-bd/2], [lx+bd,by], [rx-bd,by],    [rx-bd/2,by-bd/2], [rx-bd,by-bd],   [lx+bd,by-bd] ],
+        e:[ [lx+bd/2,cy], [lx+bd,cy+bd/2], [lx+bd,by-bd], [lx+bd/2,by-bd/2], [lx,by-bd],      [lx,cy+bd/2]  ],
+        f:[ [lx,ty+bd], [lx+bd/2,ty+bd/2], [lx+bd,ty+bd],   [lx+bd,cy-bd/2], [lx+bd/2,cy],    [lx,cy-bd/2]  ],
+        g:[ [lx+bd/2,cy], [lx+bd,cy-bd/2], [rx-bd,cy-bd/2],    [rx-bd/2,cy], [rx-bd,cy+bd/2], [lx+bd,cy+bd/2] ],
     };
+    var slide = function(array, x, y){
+        for(var i=0;i<array.length;i++){
+            array[i][0] = array[i][0] + x ;
+            array[i][1] = array[i][1] + y ;
+        }
+    }
+    slide(this.point.a, 0,        -bd*0.25);
+    slide(this.point.b,  bd*0.15, -bd*0.15);
+    slide(this.point.c,  bd*0.15,  bd*0.15);
+    slide(this.point.d, 0,         bd*0.25);
+    slide(this.point.e, -bd*0.15,  bd*0.15);
+    slide(this.point.f, -bd*0.15, -bd*0.15);
 }
-
+//}}}
 //{{{ SevenSegment.prototype.mapping = function(number){
 SevenSegment.prototype.mapping = function(number){
     switch(number){
@@ -146,7 +159,7 @@ SevenSegment.prototype.mapping = function(number){
     }
 }
 //}}}
-
+//{{{ SevenSegment.prototype.draw = function(input){
 SevenSegment.prototype.draw = function(input){
     var context = this.canvas.getContext('2d');
     context.fillStyle = "rgba(" + this.color[0] + ","+ this.color[1] + "," + this.color[2] + ",1.0" + ")";
@@ -171,4 +184,4 @@ SevenSegment.prototype.setColor = function(r,g,b){
     this.color[1] = g;
     this.color[2] = b;
 }
-
+//}}}
